2e17c94d6815b945ceab398d16210a00d00d3cca
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-09-06 11:46:29 +0530

    Loading..

---
+++ day1Intro/src/binaryTree/IntroJava.java
@@ -0,0 +1,24 @@
+package binaryTree;
+
+public class IntroJava {
+	public static void main(String[] args) {
+		
+		//public-> anyone can call
+				//static-> it will load in memory first
+				//String->any request in java consider as String
+				//only one  main method in a project
+				//void-> no return type
+				//main-> method
+				//[] ->for main method
+				//args--> value->you can write anything
+				
+				System.out.println("Abhishek Aryan Giri");
+				System.out.println(1);
+				System.out.println(2);
+				
+				//for loop
+				
+				//syso + ctrl + space
+	}
+
+}
