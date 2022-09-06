e5c4aac2c6f031ff02ad7049464df63f0b8880c6
AbhishekAryanWeb170901 <bittoogoswami@gmail.com>
2022-09-06 14:55:32 +0530

    Loading..

---
+++ day1Basic/src/day1Basic/Break.java
@@ -0,0 +1,24 @@
+package day1Basic;
+
+public class Break {
+
+	public static void main(String[] usingBreak) {
+		
+		for(int i=1;i<=3;i++)
+		{
+			for(int j=3;j>1; j--)
+//				break;
+			{
+				System.out.println(i+" "+j);
+				break;
+				//i will run onlly once
+				//break inside j so j will be out
+				//break will out the value of i+j  in every loop
+				//every time the value of i will be added once
+				//like i=1 then j=3 again
+				//i=2--> j=3
+				//i=3 --> j=3
+			}
+		}
+	}
+}
---
+++ day1Basic/src/day1Basic/Loop.java
@@ -0,0 +1,15 @@
+package day1Basic;
+
+public class Loop {
+	
+	public static void main(String[] loops) {
+		
+	//nested Loop-> loop inside loop
+	for(int i=1;i<=10;i++)
+	{
+			System.out.println("value of i : " + "  " + i);
+		}
+
+	}
+
+}
